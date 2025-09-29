0924d6b760920b283032b614e29f0a466f5b74de
Mohammed Ayad <mayad@asaltech.com>
2025-09-29 10:32:02 +0300

    disabling some entries since not fully functional

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -12,13 +12,13 @@ const Sidebar = () => {
       name: 'Dashboard', 
       path: '/', 
       icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6', 
-      active: true 
+      active: false 
     },
     { 
       name: 'Traceability Matrix', 
       path: '/matrix', 
       icon: 'M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z', 
-      active: true 
+      active: false 
     },
     { 
       name: 'Requirements', 
